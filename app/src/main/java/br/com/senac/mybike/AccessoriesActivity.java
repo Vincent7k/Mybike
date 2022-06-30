29c9e6f06067b3c1b9d339f1be95a041bd519517
lNIcxs <nicckamorim@gmail.com>
2022-06-30 17:18:57 -0300

    click listener

---
+++ app/src/main/java/br/com/senac/mybike/AccessoriesActivity.java
@@ -0,0 +1,14 @@
+package br.com.senac.mybike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccessoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accessories_layout);
+    }
+}
--- app/src/main/java/br/com/senac/mybike/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/senac/mybike/MenuPrincipalActivity.java
@@ -1,14 +1,36 @@
 package br.com.senac.mybike;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MenuPrincipalActivity extends AppCompatActivity {
+public class MenuPrincipalActivity extends AppCompatActivity implements View.OnClickListener {
+    CardView cardClients, cardReservation, cardMaintenance, cardSales, cardAccessories, cardCatalog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principa_layout);
+
+        cardClients = findViewById(R.id.cardClients);
+        cardReservation = findViewById(R.id.cardReservation);
+        cardMaintenance = findViewById(R.id.cardMaintanance);
+        cardSales = findViewById(R.id.cardSales);
+        cardAccessories = findViewById(R.id.cardAccessories);
+        cardCatalog = findViewById(R.id.cardCatalog);
+
+        cardClients.setOnClickListener(this);
+        cardReservation.setOnClickListener(this);
+        cardMaintenance.setOnClickListener(this);
+        cardSales.setOnClickListener(this);
+        cardAccessories.setOnClickListener(this);
+        cardCatalog.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
